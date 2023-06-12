0ab0272f8906ad0fdab3ae415b8cd75865eabdca
Noah Nelson <noah.james.nelson@gmail.com>
2023-06-11 22:50:53 -0400

    Added DS Store and title input array element

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,21 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: "What is your application's title name?",
+        name: 'title',
+        default: 'App Title',
+        validate: function (answer) {
+            if (answer.length < 1) {
+                return console.log("Please input a proper app title");
+            }
+            return true;
+        }
+    },
+    
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
